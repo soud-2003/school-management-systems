1e2d67bc36fe15b43ec33639fcad8cb3adc2a71e
Developer4172 <soudmohd2003@gmail.com>
2026-03-03 11:30:58 +0300

    hhh

--- backend/school_management/school_management/settings.py
+++ backend/school_management/school_management/settings.py
@@ -62,6 +62,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
+    "https://*.vercel.app",
+    "https://*.vercel.com",
+    "https://school-management-systems-inky.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
