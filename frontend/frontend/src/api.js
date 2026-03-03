7d695155998b9edfe249b6ab331a9b2d2d9ad967
Developer4172 <soudmohd2003@gmail.com>
2026-03-03 11:51:11 +0300

    fic

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://school-management-systems-deem.onrender.com/api",
 });
 
 api.interceptors.request.use(
